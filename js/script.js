ec29bce79bd7a733f228f76e89f8153ceed4e0a6
Kaenalion <91117705+Kaenalion@users.noreply.github.com>
2021-12-22 12:21:49 +0100

    Forme

--- js/script.js
+++ js/script.js
@@ -3,4 +3,10 @@
         el: document.querySelector('[data-scroll-container]'),
         smooth: true
     });
+
+    document.getElementById("anchor").onclick=function(e){
+        e.preventDefault;
+
+        scroll.scrollTo(document.querySelector('.main-header'))
+    }
 })();
