3194187d1a033b46f073d0d298e8024058cfe29d
Kaenalion <91117705+Kaenalion@users.noreply.github.com>
2022-01-09 12:59:14 +0100

    Update

--- js/script.js
+++ js/script.js
@@ -5,16 +5,21 @@
         repeat:true,
         scrollingClass:"is-scrolling"
     });
-
     document.querySelector(".anchor").onclick=function(e){
         e.preventDefault;
 
         scroll.scrollTo(document.querySelector('.main-header'))
     }
+
+    //Array des éléments pour éviter la surmutiplication des lignes de codes identiques
     let list = ['preview', 'img-details', 'blue-title', 'text-details']
     let listS= ["left-h1", "tickets-title", "online", "anchor"]
 
+//détecte le scroll et éxécute une fonction
+
     scroll.on("scroll", (args) => {
+
+//permet de changer l'opacité en fonction de la progression du scroll des éléments
 
         list.forEach(function(a){
 
@@ -23,6 +28,7 @@
                 let opacitySet = 0;
                     if(progress <= 0.5){
                     opacitySet = Math.round(progress*200)/100;
+                    console.log(opacitySet);
                     document.querySelectorAll("." + a)[0].style.opacity = opacitySet;
                     document.querySelectorAll("." + a)[1].style.opacity = opacitySet;
                     document.querySelectorAll("." + a)[2].style.opacity = opacitySet;
